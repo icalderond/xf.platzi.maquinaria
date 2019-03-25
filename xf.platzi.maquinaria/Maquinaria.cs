5226178a327edfb2d7ec42087b6f1a395bf1e86c
icalderond <icalderond@outlook.com>
2019-03-24 23:15:36 -0600

    Practice finished

---
+++ xf.platzi.maquinaria/Maquinaria.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Timers;
+namespace xf.platzi.maquinaria
+{
+    public class Maquinaria : NotificationEnabledObject
+    {
+        Timer timer;
+
+        public Maquinaria()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Elapsed += (s, a) =>
+            {
+                Hora = DateTime.Now.Hour;
+                Minuto = DateTime.Now.Minute;
+                Segundo = DateTime.Now.Second;
+            };
+            timer.Start();
+        }
+
+        private int _Hora;
+        public int Hora
+        {
+            get { return _Hora; }
+            set
+            {
+                _Hora = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _Minuto;
+        public int Minuto
+        {
+            get { return _Minuto; }
+            set
+            {
+                _Minuto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _Segundo;
+        public int Segundo
+        {
+            get { return _Segundo; }
+            set
+            {
+                _Segundo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ xf.platzi.maquinaria/NotificationEnabledObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace xf.platzi.maquinaria
+{
+    public class NotificationEnabledObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
